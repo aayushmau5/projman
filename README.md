ddac9c0e589735e97443a9325f618bdb84ec5a78
Aayush Kumar Sahu <aayushmau5@gmail.com>
2020-11-04 19:53:58 +0530

    😁 Some major progress

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # node-app-cli
+
+## TODO
+
+- [ ] show various options
+  - [x] -h, --help for help
+  - [ ] -n, --new for new project addition
+  - [ ] -d, --delete for deleting the projects(list and then delete prompt)
+  - [ ] -m, --modify for modifying the editor or something else
+- [ ] If no option is given, show the list of projects and enter to open the project in the given IDE/Editor
+- [ ] defaults with the current working directory
+- [ ] Publish on NPM
--- project.js
+++ project.js
@@ -1,9 +1,59 @@
 #!usr/bin/env node
+const fs = require('fs');
+const path = require('path');
 
 const { program } = require('commander');
+const inquirer = require('inquirer');
 
-program.option('-g --hello', 'Say Hello World');
+let projectData;
+
+const fileExists = fs.existsSync(path.join(__dirname, 'projectData.json'));
+if (fileExists) {
+    projectData = require('./projectData.json');
+}
+
+program.option('-n --new', 'Create a new entry')
+    .option('-d --delete', 'Delete an entry')
+    .option('-m --modify', 'Modify an entry');
 
 program.parse(process.argv);
 
-if (program.hello) console.log("Hello World");
+if (program.new) {
+    // making a new entry for a project
+    if (fileExists) {
+        addProject();
+    } else {
+        fs.writeFile("projectData.json", "[]", (err) => {
+            if (err) throw err;
+
+            console.log("Project data file created.");
+        });
+    }
+}
+else if (program.delete) console.log("Delete");
+else if (program.modify) console.log("Modify");
+else {
+    // when `project` is ran, show list of all the saved projects
+    if (fileExists) {
+        console.log(projectData);
+        console.log("Showing Projects");
+    } else {
+        console.log("Project data file doesn't exist.\nUse -n or --new to create and insert a project");
+    }
+}
+
+async function addProject() {
+    const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'path',
+        message: 'What will be the path to the project? 📁',
+        default: process.cwd()
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What will be the project name? 🚀',
+        default: path.basename(process.cwd())
+    }]);
+    console.log(answers);
+}
